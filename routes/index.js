0c665fa872e64000f12496b6769bd81bb65c372c
Stuart Campbell <stuart@harto.org>
2016-03-21 18:24:52 -0700

    Remove unwanted cruft

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    res.json({title: 'Battleship'});
 });
 
 module.exports = router;
